1b907e3aabaf1e13deb2493f2140c9bd1f8af33f
RAVANA-SL <85247716+RAVANA-SL@users.noreply.github.com>
2021-07-12 21:06:03 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 const Language = require('../language');
 const Lang = Language.getString('wallpaper');
 
-Asena.addCommand({pattern: 'ravana ', fromMe: false, desc: Lang.WP}, (async (message, match) => {
+Asena.addCommand({pattern: 'ravana', fromMe: false, desc: Lang.WP}, (async (message, match) => {
 
     var r_text = new Array ();
     
