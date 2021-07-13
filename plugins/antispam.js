ecfaa119104fe69d3ef2c717cb9401a1c17db6b0
RAVANA-SL <85247716+RAVANA-SL@users.noreply.github.com>
2021-07-13 15:23:50 +0530

    Antispam 😍

---
+++ plugins/antispam.js
@@ -0,0 +1,70 @@
+const Asena = require('../events');
+
+const {MessageType} = require('@adiwajshing/baileys');
+
+const Ln = ('This command for any emergency situation about any kind of WhatsApp SPAM in Group');
+
+const code = ('A\nɴ\nᴛ\nɪ\ns\nᴘ\nᴀ\nᴍ\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n*☮️RAVANA☮️*')
+const FINAL = "THIS IS AN ANTISAPM (anti lag),\nමෙම කේතය ඔබගේ දුරකථනය හදිසි ස්පෑම් එල්ල වීමකදි  සිරවිම වලකාලයි"
+const TMUTE = "Trying to close temporary,\nතාවකාලිකව සමුහය වැසීමට උත්සාහ කිරීම"
+const MUT = ".mute 1d"
+const SCXR = "Script එක ක්‍රියාත්මක වෙමින් පවතී...."
+Asena.addCommand({pattern: 'antispam', fromMe: true, deleteCommand: true, desc: Ln,}, (async (message, match) => {
+
+  var msg = await message.reply('Preforming....');
+
+  await message.client.sendMessage(
+    message.jid,TMUTE, MessageType.text);
+
+    await message.client.sendMessage(
+      message.jid,MUT, MessageType.text);
+
+      await message.client.sendMessage(
+        message.jid,SCXR, MessageType.text);
+
+        await message.client.sendMessage(
+          message.jid,code, MessageType.text);
+
+    await message.client.sendMessage(
+        message.jid,code, MessageType.text);
+
+        await message.client.sendMessage(
+            message.jid,code, MessageType.text);
+
+            await message.client.sendMessage(
+                message.jid,code, MessageType.text);
+
+                await message.client.sendMessage(
+                  message.jid,code, MessageType.text);
+
+                  await message.client.sendMessage(
+                    message.jid,code, MessageType.text);
+
+                    await message.client.sendMessage(
+                      message.jid,code, MessageType.text);
+
+                      await message.client.sendMessage(
+                        message.jid,code, MessageType.text);
+  
+                               await message.client.sendMessage(
+            message.jid,code, MessageType.text);
+
+            await message.client.sendMessage(
+                message.jid,code, MessageType.text);
+
+                await message.client.sendMessage(
+                  message.jid,code, MessageType.text);
+
+                  await message.client.sendMessage(
+                    message.jid,code, MessageType.text);
+
+                    await message.client.sendMessage(
+                      message.jid,code, MessageType.text);
+
+                      await message.client.sendMessage(
+                        message.jid,code, MessageType.text);
+  
+                          await message.client.sendMessage(
+                             message.jid,FINAL, MessageType.text);
+
+  }));
