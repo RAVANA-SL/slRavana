bf5b9f86ce7906951a1d87b8a7b731987c834d23
RAVANA-SL <85247716+RAVANA-SL@users.noreply.github.com>
2021-08-14 13:45:26 +0530

    💠RAVANA💠
    💌RAVANA NEW UPDATE💌
    
    ⭕⭕⭕New version 3.5⭕⭕⭕
    
    💥add new logo pack .upack
    
    💥add error fixed bot
    
    💥add meme pack
    
    💥add new ai bot
    
    💥bug fixed 
    
    💥you can change downloading song , uplaoding song
    
    💥add auto bio
    
    💥add anti link 
    
    🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹

---
+++ plugins/upack.js
@@ -0,0 +1,136 @@
+/*💠 විදානය : ```.devil``` 
+Makes  Logo._
+🔖 උදාහරණ: **.devil Ravana*
+
+💠 විදානය : ```.bear``` 
+💢 Description : Makes  Logo._
+🔖 උදාහරණ: **.bear Ravana*
+
+💠 විදානය : ```.thunder``` 
+💢 Description : Makes  Logo._
+🔖 උදාහරණ: **.thunder Ra;One*
+
+💠 විදානය : ```.123glitch```
+💢 Description : Makes  Logo._
+🔖 උදාහරණ: **.123glitch Ravana*
+
+💠 විදානය : ```.berry``` 
+💢 Description : Makes  Logo._
+🔖 උදාහරණ: **.berry Ravana*
+
+💠 විදානය : ```.peridot``` 
+💢 Description : Makes  Logo._
+🔖 උදාහරණ: **.light Ravana*
+
+💠 විදානය : ```.joker``` 
+💢 Description : Makes  Logo._
+🔖 උදාහරණ: **.joker Ravana*
+
+💠 විදානය : ```.honey``` 
+💢 Description : Makes  Logo._
+🔖 උදාහරණ: **.ninja Ravana*
+
+💠 විදානය : ```.glitter``` 
+💢 Description : Makes  Logo_
+🔖 උදාහරණ: **.glitter Ravana*
+
+💠 විදානය : ```.bokeh``` 
+💢 Description : Makes  Logo._
+🔖 උදාහරණ: **.bokeh Ra;One*
+
+💠 විදානය : ```.greenhorror``` 
+💢 Description : Makes  Logo._
+🔖 උදාහරණ: **.greenhorror Ravana*
+
+💠 විදානය : ```.2marvel``` 
+💢 Description : Makes  Logo._
+🔖 උදාහරණ: **.2marvel Ravana*
+
+💠 විදානය : ```.d3neon``` 
+💢 Description : Makes  Logo._
+🔖 උදාහරණ: **.3dneon Ra;One*
+
+💠 විදානය : ```.metalgalaxy``` 
+💢 Description : Makes  Logo._
+🔖 උදාහරණ: **.metalgalaxy Ra;One*
+
+💠 විදානය : ```.metalsilver``` 
+💢 Description : Makes  Logo._
+🔖 උදාහරණ: **.metalsilver Ra;One*
+
+💠 විදානය : ```.2graf``` 
+💢 Description : Makes  Logo._
+🔖 උදාහරණ: **.2graf Ravana*
+
+💠 විදානය : ```.rosegold``` 
+💢 Description : Makes  Logo._
+🔖 උදාහරණ: **.rosegold sl;Ravana*
+
+💠 විදානය : ```.transformer``` 
+💢 Description : Makes  Logo._
+🔖 උදාහරණ: **.transformer Ravana*
+
+💠 විදානය : ```.ice``` 
+💢 Description : Makes  Logo._
+🔖 උදාහරණ: **.ice Ravana*
+
+💠 විදානය : ```.luxurymetel``` 
+💢 Description : Makes  Logo._
+🔖 උදාහරණ: **.luxurymetel Ra;One*
+
+💠 විදානය : ```.mtg``` 
+💢 Description : Makes  Logo._
+🔖 උදාහරණ: **..mtg Ravana*
+
+💠 විදානය : ```.camarica``` 
+💢 Description : Makes  Logo._
+🔖 උදාහරණ: **.camarica Ravana*
+
+💠 විදානය : ```.magma``` 
+💢 Description : Makes  Logo._
+🔖 උදාහරණ: **.magma Ravana*
+
+💠 විදානය : ```.layered``` 
+💢 Description : Makes  Logo._
+🔖 උදාහරණ: **.layered Ravana*
+
+💠 විදානය : ```.embossed``` 
+💢 Description : Makes  Logo._
+🔖 උදාහරණ: **.embossed Ravana*
+
+💠 විදානය : ```.papercut``` 
+💢 Description : Makes  Logo._
+🔖 උදාහරණ: **.papercut Ravana*
+
+💠 විදානය : ```.wonderfullgraf``` 
+💢 Description : Makes  Logo_
+🔖 උදාහරණ: **.wonderfullgraf Ra;One*
+
+💠 විදානය : ```.criholy``` 
+💢 Description : Makes  Logo._
+🔖 උදාහරණ: **.criholy Ravana*
+
+💠 විදානය : ```.reacloud``` 
+💢 Description : Makes  Logo._
+🔖 උදාහරණ: **.reacloud Ravanan
+💠 විදානය : ```.catwindow``` 
+💢 Description : Makes  Logo_
+🔖 උදාහරණ: **.catwindow Ravana*
+
+💠 විදානය : ```.sci``` 
+💢 Description : Makes  Logo_
+🔖 උදාහරණ: **.scc Ravana*
+
+💠 විදානය : ```.halloween``` 
+💢 Description : Makes  Logo_
+🔖 උදාහරණ: **.scc Ravana*
+
+💠 විදානය : ```.glue``` 
+💢 Description : Makes  Logo_
+🔖 උදාහරණ: **.scc Ravana*
+
+💠 විදානය : ```.cup``` 
+💢 Description : Makes  Logo._
+🔖 උදාහරණ: **.cup Ravana*
+
+*/
